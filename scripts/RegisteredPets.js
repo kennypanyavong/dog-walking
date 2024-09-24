2295d36e0d8b62bbbf02cb10f8e64b4ade845af7
Kenny Panyavong <elp.kenny@gmail.com>
2024-09-24 14:36:41 -0500

    Added click event listener to RegisteredPets.js

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,11 +1,26 @@
-import { getPets } from "./database.js"
+import { getPets, getWalkers } from "./database.js"
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const clickTarget = clickEvent.target
+        const walkerId = clickTarget.dataset.walkerforeignkey
+
+        const allWalkers = getWalkers()
+        for (const walker of allWalkers) {
+            if (walker.id === parseInt(walkerId)) {
+                window.alert(`This pet is being walked by ${walker.name}`)
+            }
+        }
+    }
+)
 
 export const RegisteredPets = () => {
     const pets = getPets()
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li data-walkerForeignKey="${pet.walkerId}">${pet.name}</li>`
     }
 
     petHTML += "</ul>"
